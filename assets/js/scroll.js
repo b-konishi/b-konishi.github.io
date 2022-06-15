d6be66a6e8cecccff7cd5cebef5766daa6b892ad
b-konishi <konishi5282@gmail.com>
2022-06-15 14:18:23 +0900

    update design and introduce jQuery

---
+++ assets/js/scroll.js
@@ -0,0 +1,9 @@
+
+$('a[href*="#"]').click(function () {
+
+	var elmHash = $(this).attr('href'); 
+	var pos = $(elmHash).offset().top;	
+	$('body,html').animate({scrollTop: pos}, 500); 
+	return false;
+
+});
